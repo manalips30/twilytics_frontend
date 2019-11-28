42e8ab246e2e6dd6cba49590d0ef5fc0b5efbbf2
mana <manalips30@gmail.com>
2019-11-27 22:21:24 -0500

    Added Date Filter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Form from "./Form";
 import Contacts from "./components/contacts";
+import Filter from "./components/filter";
 import "./App.css";
 
 
@@ -8,8 +9,7 @@ class App extends Component {
   state = {
     searchKey: "",
     fields: {},
-    contacts :[
-    ]
+    contacts :[],
   };
 
 //http://localhost:8080/list?name=Donald
@@ -25,9 +25,11 @@ class App extends Component {
   };
 
   //http://twilytics.us-east-2.elasticbeanstalk.com/list?name=donald  
-
+  //http://twilytics.us-east-2.elasticbeanstalk.com/list?name=${search}
+  
   onSearch = (search) => {
-    fetch(`http://twilytics.us-east-2.elasticbeanstalk.com/list?name=${search}`)
+    console.log("onSearch called")
+    fetch(`http://localhost:8080/list?name=${search}`)
     .then(res => res.json())
     .then((data) => {
       this.setState({ contacts: data })
@@ -38,12 +40,23 @@ class App extends Component {
       })
   };
 
+  onDate = (startDate, endDate) => {
+    console.log("onDate called")
+    fetch('http://localhost:8080/list?name=Donald')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ contacts: data })
+    })
+    .catch(console.log)
+  }
+
   render() {
     const  {searchKey} = this.state;
     return (
       <div className="App">
         <Form onChange={this.onChange} onSearch={this.onSearch} />
         <Contacts contacts={this.state.contacts} searchKey={searchKey}/>
+        <Filter onDate={this.onDate} />
       </div>
     );
   }
--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,22 +1,31 @@
 import React from 'react'
 import "./contacts.css"; 
 
+
 const Contacts = ({ contacts, searchKey }) => {
   console.log(contacts)
   return (
     <div>
-      <h1>Search Result</h1>
-        <p>
-            {JSON.stringify(searchKey, null, 2)}
-        </p>
         <div className='cardWrapper'>
       {contacts.map((contact) => (
-        
         <div className="card" key={contact.poi_name}>
          <div className="card-body">
-            <h5 className="card-title">{contact.poi_name}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{contact.created_at}</h6>
-            <h6 className="card-subtitle mb-2 text-muted">{contact["user.location"]}</h6>
+            <div className="card-title">
+              <img className="profilepicture"
+              src={contact["user.profile_image_url_https"]}
+              alt="new"
+              />
+            </div>
+            <div className = "card-title">
+              <h5 className="title">{contact.poi_name}</h5>
+            </div>
+            <div className = "card-title">
+              <h5 className="emoji">{contact.sentiment}</h5>
+            </div>
+            <div className = "card-title">
+              <h5 className="date">{contact.created_at}</h5>
+            </div>
+            <h6 className="location">{contact["user.location"]}</h6>
             <p className="card-text">{contact.full_text}</p>
           </div>
         </div>
