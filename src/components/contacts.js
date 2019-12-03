1f39e0c019965a25a0fc439eb070838cbe9c11b4
Mythreyi P <pmythreyi@gmail.com>
2019-12-03 00:21:25 -0800

    Styling with css

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./contacts.css"; 
 
+
 const Contacts = ({ contacts, searchKey }) => {
   console.log(contacts)
   return (
@@ -8,7 +9,8 @@ const Contacts = ({ contacts, searchKey }) => {
         <div className='cardWrapper'>
       {contacts.map((contact) => (
         <div className="card" key={contact.poi_name}>
-         <div className="card-body">
+          <div className={contact.sentiment == 'positive' ? "green" : contact.sentiment == 'negative' ? "red" : "blue" } >
+         <div className="card-body" >
             <div className="card-title">
               <img className="profilepicture"
               src={contact.userProfileImageUrlHttps}
@@ -19,13 +21,16 @@ const Contacts = ({ contacts, searchKey }) => {
               <h5 className="title">{contact.userName}</h5>
             </div>
             <div className = "card-title">
-              <h5 className="emoji">{contact.sentiment}</h5>
+              
+              <h5 className="emoji" >{contact.sentiment}</h5>
+
             </div>
             <div className = "card-title">
               <h5 className="date">{contact.createdAt}</h5>
             </div>
             <h6 className="location">{contact.state}</h6>
             <cardWrapper className="buttonTweet" onClick={()=> window.open(`https://twitter.com/TulsiGabbard/status/${contact.id}`, "_blank")} >{contact.fullText}</cardWrapper>
+          </div>
           </div>
         </div>
       ))
